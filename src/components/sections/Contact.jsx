7afa1e49f789f0bfb6c4c8dcf0565d0b8a799760
bhumitgoyal <lifestyleprojects.rg@gmail.com>
2025-02-24 18:35:57 +0530

    first commit

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -22,7 +22,7 @@ export const Contact =() => {
     return <section id="contact" className="min-h-screen flex items-senter justify-center py-20">
 
         <RevealOnScroll>
-            <div className="px-4 w-150">
+            <div className="px-4 w-full md:min-w-[400px] md:max-w-lg lg:min-w-[500px] lg:max-w-xl">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                     Get In Touch
                 </h2> 
